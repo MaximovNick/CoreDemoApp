e26212a0e8dbeac100ccdd84d412a81d241f9c10
Nikolai Maximov <kolkamaximov@gmail.com>
2022-07-13 13:59:59 +0300

    add 2 buttons

--- CoreDemoApp/TaskViewController.swift
+++ CoreDemoApp/TaskViewController.swift
@@ -15,12 +15,35 @@ class TaskViewController: UIViewController {
         textField.borderStyle = .roundedRect
         return textField
     }()
+    
+    private lazy var saveButton: UIButton = {
+        createButton(
+            withTittle: "Save Task",
+            andColor: UIColor(
+                red: 21/255,
+                green: 101/255,
+                blue: 192/255,
+                alpha: 194/255
+            ),
+            action: UIAction { _ in
+                self.dismiss(animated: true)
+            })
+    }()
+    
+    private lazy var cancelButton: UIButton = {
+        createButton(
+            withTittle: "Cancel",
+            andColor: .systemRed,
+            action: UIAction { _ in
+                self.dismiss(animated: true)
+            })
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .white
-       setupSubviews(taskTextField)
+       setupSubviews(taskTextField, saveButton, cancelButton)
         setConstraints()
         
         
@@ -40,6 +63,33 @@ class TaskViewController: UIViewController {
             taskTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
             taskTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
         ])
+        
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            saveButton.topAnchor.constraint(equalTo: taskTextField.bottomAnchor, constant: 20),
+            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+        
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            cancelButton.topAnchor.constraint(equalTo: saveButton.bottomAnchor, constant: 20),
+            cancelButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            cancelButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40)
+        ])
+    }
+    
+    private func createButton(withTittle tittle: String, andColor color: UIColor, action: UIAction) -> UIButton {
+        var attributes = AttributeContainer()
+        attributes.font = UIFont.boldSystemFont(ofSize: 18)
+        
+        var buttonConfiguration = UIButton.Configuration.filled()
+        buttonConfiguration.baseBackgroundColor = color
+        buttonConfiguration.attributedTitle = AttributedString(tittle, attributes: attributes)
+        
+        return UIButton(configuration: buttonConfiguration, primaryAction: action)
     }
 
 }
